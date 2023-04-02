75172e5454d363b185fdbd37d76fb962717f5cfc
ybitake <ybitake@paypal.com>
2023-04-02 15:52:13 +0530

    use ReactMarkdown instead of dangerously set html

--- lib/blogs.ts
+++ lib/blogs.ts
@@ -1,8 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
-import { remark } from 'remark';
-import html from 'remark-html';
 
 const blogsDirectory = path.join(process.cwd(), 'blogs');
 
@@ -112,17 +110,11 @@ export async function getBlogData(id) {
 
     // Use gray-matter to parse the blog metadata section
     const matterResult = matter(fileContents);
-
-    // Use remark to convert markdown into HTML string
-    const processedContent = await remark()
-        .use(html)
-        .process(matterResult.content);
-    const contentHtml = processedContent.toString();
-
-    // Combine the data with the id and contentHtml
+    const content = `${matterResult.content}`;
+    // Combine the data with the id and content
     return {
         id,
-        contentHtml,
+        content,
         ...matterResult.data,
     };
 }
